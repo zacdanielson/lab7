ad273ae90ec397af024247f859e31936fcd35d8c
Zac Danielson <zacdanielson32@gmail.com>
2016-03-08 01:24:09 +0000

    added all file

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.sendFile('weather.html', { root: 'public' });
+});
+
+router.get('/getcity',function(req,res,next) { 
+	console.log("in getcity route");
+	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+		  if(err) throw err;
+		   var cities = data.toString().split("\n");
+		   var jsonresult = [];
+		   var myRe = new RegExp("^" + req.query.q);
+		   for(var i = 0; i < cities.length; i++) {
+			   var result = cities[i].search(myRe);
+			   if(result !=-1) {
+				   console.log(cities[i]);
+				   jsonresult.push({city:cities[i]});
+			   }
+		   }
+		   res.status(200).json(jsonresult);
+	 })
+});
+
+module.exports = router;
